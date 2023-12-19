3e692a606d13cdabd8ae4f0e5fd79c1cd6113205
Suresh <chakali.suresh@capgemini.com>
2023-12-19 15:24:53 +0530

    MultiThreading in copy9

---
+++ Multithreading/src/com/cg/RunnabelIntrface/MultiThreadingDemo.java
@@ -0,0 +1,18 @@
+package com.cg.RunnabelIntrface;
+
+//the Runnable Interface
+class MultiThreadingDemo implements Runnable {
+	public void run()
+	{
+		try {
+			// Displaying the thread that is running
+			System.out.println(
+				"Thread " + Thread.currentThread().getId()
+				+ " is running");
+		}
+		catch (Exception e) {
+			// Throwing an exception
+			System.out.println("Exception is caught");
+		}
+	}
+}
