c927c996dcdfd6c67f02f904acc05f61fcc89bc2
Suresh <chakali.suresh@capgemini.com>
2023-12-19 15:30:26 +0530

    from in copy11

---
+++ Method Overriding/src/com/cg/Overriding/Animal.java
@@ -0,0 +1,8 @@
+package com.cg.Overriding;
+
+public class Animal {
+	void speak() {
+		System.out.println("The animal speaks");
+	}
+
+}
---
+++ Method Overriding/src/com/cg/Overriding/Main.java
@@ -0,0 +1,15 @@
+package com.cg.Overriding;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal animal = new Animal();
+		Dog dog = new Dog();
+		dog.speak();
+		animal.speak();
+		
+
+	}
+
+}
