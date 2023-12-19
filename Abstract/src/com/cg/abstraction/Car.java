228a9291ad9c44a63c5aa194ad7cda8a08817e56
Suresh <chakali.suresh@capgemini.com>
2023-12-19 14:00:57 +0530

    Abstract in copy5

---
+++ Abstract/src/com/cg/abstraction/Car.java
@@ -0,0 +1,15 @@
+package com.cg.abstraction;
+
+public  class Car extends Vechical {
+
+	public static void main(String[] args) {
+	}
+
+
+	@Override
+	void go() {
+		System.out.println("Drive  the car");
+		
+	}
+
+}
---
+++ Abstract/src/com/cg/abstraction/Main.java
@@ -0,0 +1,11 @@
+package com.cg.abstraction;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.go();
+
+	}
+
+}
