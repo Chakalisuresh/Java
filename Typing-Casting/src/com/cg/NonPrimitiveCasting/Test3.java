810cc38bafd247f9780fc6b37f8dd1f1bc9f154f
Suresh <chakali.suresh@capgemini.com>
2024-01-27 03:21:50 +0530

    Type casting

---
+++ Typing-Casting/src/com/cg/NonPrimitiveCasting/Test3.java
@@ -0,0 +1,18 @@
+package com.cg.NonPrimitiveCasting;
+
+import com.cg.NonPrimitiveCasting.Father3.Son;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// not work
+		Father3 father;
+		//father = new Son();
+//		Son son = new Son()father; //Down Casting
+//		son.work();
+//		son.play();
+
+	}
+
+}
