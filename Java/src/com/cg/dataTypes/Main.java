9d19543ce9b66c7246d57770d540df917c6f7aef
Chakalisuresh <121609226+Chakalisuresh@users.noreply.github.com>
2024-01-30 16:38:14 +0530

    Add files via upload

---
+++ Java/src/com/cg/dataTypes/Main.java
@@ -0,0 +1,32 @@
+package com.cg.dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x =3;
+		float y = 30.2f;
+		long  z = 757684839l;
+		double xx = 23.09d;
+		boolean yy = true ;
+		char zz = '@';
+		String s = "suri";
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(xx);
+		System.out.println(yy);
+		System.out.println(zz);
+		System.out.println(s);
+		
+	         int i;
+	         i=5;
+	        
+	              int ii = 6;
+	              System.out.print(i+" " +ii);
+	         
+	}
+}
+	
+
+
